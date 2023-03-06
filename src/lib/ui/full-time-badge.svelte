021989d8e3d13b87f6f4a984fbd5a97b1dea2f3f
Sergei Kononov <kobo220376@gmail.com>
2023-03-06 22:37:19 +0300

    added full time badge

---
+++ src/lib/ui/full-time-badge.svelte
@@ -0,0 +1,14 @@
+<p>Full time</p>
+
+<style lang="scss">
+	p {
+		font-family: 'Roboto';
+		font-weight: 700;
+		font-size: 12px;
+		color: #334680;
+		padding: 6px 8px;
+		border: 1px solid #334680;
+		border-radius: 4px;
+		width: fit-content;
+	}
+</style>
--- src/routes/job/[slug]/+page.svelte
+++ src/routes/job/[slug]/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import BackButton from '$lib/button/back-button.svelte';
+	import FullTimeBadge from '$lib/ui/full-time-badge.svelte';
 </script>
 
 <h1>Hello and welcome to my site!</h1>
 <BackButton />
+<FullTimeBadge />
